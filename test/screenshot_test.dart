45e1e9de7a60ce98de567c57fbaf3aacfa62b236
Xavier H <xavier.hainaux@gmail.com>
2020-02-04 19:18:45 +0100

    Use Flutter golden infrastructure (#7)

--- test/screenshot_test.dart
+++ test/screenshot_test.dart
@@ -1,16 +1,13 @@
 import 'dart:io';
-import 'dart:typed_data';
-import 'dart:ui';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:lottie/lottie.dart';
 import 'package:path/path.dart' as p;
-import 'utils/utils_golden.dart';
 
 void main() {
   //TODO(xha): download all screenshots from lottie-android as golden screenshots
   // https://happo.io/api/a/27/p/27/reports/abe43ed3d2f25a3ea3c38b1740011cf84fea5e93-android28
 
-  final sampleFiles = 'example/assets';
+  final animationPath = 'example/assets';
 
   var animations = <_Screenshot>[
     _Screenshot('AndroidWave.json'),
@@ -23,18 +20,21 @@ void main() {
     _Screenshot('lottiefiles/atm_link.json', progress: 1.0),
   ];
   for (var animation in animations) {
-    test('Screenshot ${animation.name} at ${animation.progress}', () async {
-      var result = await screenshot(p.join(sampleFiles, animation.name),
-          (drawable, canvas) {
-        drawable.draw(canvas, Offset.zero & drawable.size,
-            progress: animation.progress);
-      });
+    testWidgets('Screenshot ${animation.name} at ${animation.progress}',
+        (WidgetTester tester) async {
+      var composition = await LottieComposition.fromBytes(
+          File(p.join(animationPath, animation.name)).readAsBytesSync());
 
       var goldenName = animation.goldenName;
-      expect(
-          result,
-          equalsGolden(p.join(
-              'test/golden', p.dirname(animation.name), '$goldenName.png')));
+
+      await tester.pumpWidget(RawLottie(
+        composition: composition,
+        progress: animation.progress,
+      ));
+      await expectLater(
+          find.byType(RawLottie),
+          matchesGoldenFile(
+              p.join('golden', p.dirname(animation.name), '$goldenName.png')));
     });
   }
 }
@@ -47,29 +47,4 @@ class _Screenshot {
 
   String get goldenName =>
       '${p.basenameWithoutExtension(name)}_${progress.toStringAsFixed(1).replaceAll('.', '_')}';
-}
-
-Future<Uint8List> screenshot(String compositionPath,
-    void Function(LottieDrawable, Canvas) drawCallback) async {
-  var drawable = LottieDrawable(await LottieComposition.fromBytes(
-      File(compositionPath).readAsBytesSync()));
-  var size = drawable.size;
-
-  var recorder = PictureRecorder();
-  var canvas = Canvas(recorder);
-  drawCallback(drawable, canvas);
-  var picture = recorder.endRecording();
-  var image = await picture.toImage(size.width.ceil(), size.height.ceil());
-  return (await image.toByteData(format: ImageByteFormat.png))
-      .buffer
-      .asUint8List();
-}
-
-Future<Uint8List> filmStrip(
-    String compositionPath, void Function(LottieDrawable, Canvas) drawCallback,
-    {int frameRate}) async {
-  //TODO(xha): loop for each progress (drawable.duration * frameRate)
-  // translate the canvas and draw the frame time above
-
-  throw UnimplementedError();
 }
