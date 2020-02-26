fc683482db8a37df1511c18817f9ffaf87ae0173
Xavier H <xavier.hainaux@gmail.com>
2020-02-26 23:00:28 +0100

    Add an example to run an animation once (#21)

---
+++ example/lib/examples/run_once.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
+import 'package:lottie/lottie.dart';
+import '../src/all_files.g.dart';
+
+void main() async {
+  Logger.root
+    ..level = Level.ALL
+    ..onRecord.listen(print);
+
+  runApp(App());
+}
+
+class App extends StatefulWidget {
+  const App({Key key}) : super(key: key);
+
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> with TickerProviderStateMixin {
+  int _index = 0;
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(vsync: this)
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          setState(() {
+            ++_index;
+          });
+        }
+      });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      color: Colors.lightBlue,
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue,
+        appBar: AppBar(
+          title: Text('$_index'),
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                Lottie.asset(files[_index % files.length],
+                    controller: _animationController, onLoaded: (composition) {
+                  _animationController
+                    ..duration = composition.duration
+                    ..reset()
+                    ..forward();
+                }),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
