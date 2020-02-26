1b357ba83938b9ab513ed1b12b2ab1580922e1e5
Xavier Hainaux <xavier.hainaux@gmail.com>
2020-02-26 22:53:53 +0100

    Add an example to run an animation once

---
+++ example/lib/examples/animate_no_repeat.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
+import 'package:lottie/lottie.dart';
+
+void main() async {
+  runApp(App());
+}
+
+class App extends StatefulWidget {
+  const App({Key key}) : super(key: key);
+
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> with TickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      color: Colors.lightBlue,
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue,
+        appBar: AppBar(
+          title: Text(''),
+        ),
+        body: Center(
+          child: SizedBox(
+            width: 300,
+            height: 300,
+            child: Lottie.asset(
+              'assets/LottieLogo1.json',
+              animate: true,
+              repeat: false,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
