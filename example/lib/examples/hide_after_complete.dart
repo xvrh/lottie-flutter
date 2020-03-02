2914caf2208ee024f3a9a7b2dbd35f1adf1dda45
Xavier Hainaux <xavier.hainaux@gmail.com>
2020-03-02 22:18:52 +0100

    Add example for hide after complete

---
+++ example/lib/examples/hide_after_complete.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+void main() async {
+  runApp(App());
+}
+
+class App extends StatefulWidget {
+  const App({Key key}) : super(key: key);
+
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> with TickerProviderStateMixin {
+  AnimationController _animationController;
+  bool _showAnimation = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(vsync: this)
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          setState(() {
+            _showAnimation = false;
+          });
+        }
+      });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      color: Colors.lightBlue,
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue,
+        appBar: AppBar(
+          title: Text('Show lottie animation: $_showAnimation'),
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                if (_showAnimation)
+                  Lottie.asset(
+                    'assets/LottieLogo1.json',
+                    controller: _animationController,
+                    width: 200,
+                    onLoaded: (composition) {
+                      _animationController
+                        ..duration = composition.duration
+                        ..reset()
+                        ..forward();
+                    },
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
