fb01bd529aa45c46bf8e37a654bdaa532da8713d
Xavier Hainaux <xavier.hainaux@gmail.com>
2020-04-01 23:22:35 +0200

    format

--- example/test/dynamic_properties_test.dart
+++ example/test/dynamic_properties_test.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:lottie/lottie.dart';
-
 import 'utils.dart';
 
 void main() {
