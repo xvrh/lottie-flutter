912774b9866fec10fb7e94d613b5c1c422f5409c
Xavier Hainaux <xavier.hainaux@gmail.com>
2020-12-12 22:19:37 +0100

    Update golden

---
+++ example/lib/examples/repeat.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
+  AnimationController _controller;
+  int _repeatIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(vsync: this)
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          setState(() {
+            _repeatIndex++;
+          });
+          if (_repeatIndex < 5) {
+            _controller.reset();
+            _controller.forward();
+          }
+        }
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView(
+          children: [
+            Lottie.asset(
+              'assets/AndroidWave.json',
+              controller: _controller,
+              width: 150,
+              height: 150,
+              onLoaded: (composition) {
+                // Configure the AnimationController with the duration of the
+                // Lottie file and start the animation.
+                _controller.duration = composition.duration;
+                _controller.forward();
+              },
+            ),
+            Center(child: Text('Repeat: $_repeatIndex')),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
