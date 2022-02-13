c60d5fb40ec3e85faae94f661fe52f16e1595a0e
pacowong <paco.pkwong@gmail.com>
2022-02-13 22:45:52 +0800

    Decopule draw on canvas from draw on context

--- lib/src/render_lottie.dart
+++ lib/src/render_lottie.dart
@@ -195,12 +195,16 @@ class RenderLottie extends RenderBox {
   void performLayout() {
     size = _sizeForConstraints(constraints);
   }
-
+  
   @override
   void paint(PaintingContext context, Offset offset) {
+    paintOnCanvas(context.canvas, offset);
+  }
+
+  void paintOnCanvas(Canvas canvas, Offset offset) {
     if (_drawable == null) return;
 
-    _drawable!.draw(context.canvas, offset & size,
+    _drawable!.draw(canvas, offset & size,
         fit: _fit, alignment: _alignment.resolve(TextDirection.ltr));
   }
 
