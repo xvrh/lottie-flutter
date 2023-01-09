9184bd626c6f001dda94d882cf81d42731d6e1cd
Xavier Hainaux <xavier.hainaux@gmail.com>
2023-01-09 16:40:13 +0100

    Gradient

--- lib/src/model/animatable/animatable_gradient_color_value.dart
+++ lib/src/model/animatable/animatable_gradient_color_value.dart
@@ -1,11 +1,47 @@
 import '../../animation/keyframe/gradient_color_keyframe_animation.dart';
+import '../../value/keyframe.dart';
 import '../content/gradient_color.dart';
 import 'base_animatable_value.dart';
 
 class AnimatableGradientColorValue
     extends BaseAnimatableValue<GradientColor, GradientColor> {
-  AnimatableGradientColorValue.fromKeyframes(super.keyframes)
-      : super.fromKeyframes();
+  AnimatableGradientColorValue.fromKeyframes(List<Keyframe<GradientColor>>keyframes)
+      : super.fromKeyframes(_ensureInterpolatableKeyframes(keyframes));
+
+  static List<Keyframe<GradientColor>> _ensureInterpolatableKeyframes(List<Keyframe<GradientColor>> keyframes) {
+    for (int i = 0; i < keyframes.length; i++) {
+      keyframes[i] = _ensureInterpolatableKeyframe(keyframes[i]);
+    }
+    return keyframes;
+  }
+
+  static Keyframe<GradientColor> _ensureInterpolatableKeyframe(Keyframe<GradientColor> keyframe) {
+    var startValue = keyframe.startValue;
+    var endValue = keyframe.endValue;
+    if (startValue == null || endValue == null || startValue.positions.length == endValue.positions.length) {
+      return keyframe;
+    }
+    var mergedPositions = _mergePositions(startValue.positions, endValue.positions);
+    // The start/end has opacity stops which required adding extra positions in between the existing colors.
+    return keyframe.copyWith(startValue.copyWithPositions(mergedPositions), endValue.copyWithPositions(mergedPositions));
+  }
+
+  static List<double> _mergePositions(List<double> startPositions, List<double> endPositions) {
+  var mergedArray = List<double>.filled(startPositions.length + endPositions.length, 0);
+  mergedArray.setRange(0, startPositions.length, startPositions);
+  mergedArray.setRange(startPositions.length, startPositions.length + endPositions.length, endPositions);
+  mergedArray.sort();
+  var uniqueValues = 0;
+  var lastValue = double.nan;
+  for (var i = 0; i < mergedArray.length; i++) {
+  if (mergedArray[i] != lastValue) {
+  mergedArray[uniqueValues] = mergedArray[i];
+  uniqueValues++;
+  lastValue = mergedArray[i];
+  }
+  }
+  return mergedArray.take(uniqueValues).toList();
+  }
 
   @override
   GradientColorKeyframeAnimation createAnimation() {
--- lib/src/model/content/gradient_color.dart
+++ lib/src/model/content/gradient_color.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+import '../../utils/collection.dart';
 import '../../utils/gamma_evaluator.dart';
 
 class GradientColor {
@@ -20,5 +21,34 @@ class GradientColor {
       colors[i] =
           GammaEvaluator.evaluate(progress, gc1.colors[i], gc2.colors[i]);
     }
+  }
+
+  GradientColor copyWithPositions(List<double> positions) {
+  var colors = List<Color>.filled(positions.length, const Color(0x00000000));
+  for (var i = 0; i < positions.length; i++) {
+  colors[i] = _getColorForPosition(positions[i]);
+  }
+  return GradientColor(positions, colors);
+  }
+
+  Color _getColorForPosition(double position) {
+    var existingIndex = binarySearch(positions, position);
+    if (existingIndex >= 0) {
+      return colors[existingIndex];
+    }
+    // binarySearch returns -insertionPoint - 1 if it is not found.
+    var insertionPoint = -(existingIndex + 1);
+    if (insertionPoint == 0) {
+      return colors[0];
+    } else if (insertionPoint == colors.length - 1) {
+      return colors[colors.length - 1];
+    }
+    var startPosition = positions[insertionPoint - 1];
+    var endPosition = positions[insertionPoint];
+    var startColor = colors[insertionPoint - 1];
+    var endColor = colors[insertionPoint];
+
+    var fraction = (position - startPosition) / (endPosition - startPosition);
+    return GammaEvaluator.evaluate(fraction, startColor, endColor);
   }
 }
