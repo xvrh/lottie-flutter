efc91710d8f48e43863459ae8bded74a9a48d0d3
Xavier Hainaux <xavier.hainaux@gmail.com>
2026-09-07 16:13:51 +0200

    Remove imports of 'cupertino.dart' and 'material.dart'

--- lib/src/providers/lottie_provider.dart
+++ lib/src/providers/lottie_provider.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter/foundation.dart';
 import '../../lottie.dart';
 import 'load_image.dart';
--- lib/src/render_cache.dart
+++ lib/src/render_cache.dart
@@ -1,5 +1,5 @@
 import 'dart:ui';
-import 'package:flutter/material.dart' show RenderBox;
+import 'package:flutter/widgets.dart' show RenderBox;
 import '../lottie.dart';
 import 'render_cache/store_drawing.dart';
 import 'render_cache/store_raster.dart';
